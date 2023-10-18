1053b97145848fbda850a1e0abdd9c3e20a4e6c3
haojunjie <1373169681@qq.com>
2023-10-18 21:19:16 +0800

    初始化项目

---
+++ src/main/java/com/jiem/speed/SpeedUpApplication.java
@@ -0,0 +1,20 @@
+package com.jiem.speed;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 提速
+ *
+ * @author: haojunjie
+ * @date: 2023/10/18 21:08
+ */
+@Slf4j
+@SpringBootApplication
+public class SpeedUpApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpeedUpApplication.class, args);
+        log.info("(♥◠‿◠)ﾉﾞ 启动成功   ლ(´ڡ`ლ)ﾞ  ");
+    }
+}
