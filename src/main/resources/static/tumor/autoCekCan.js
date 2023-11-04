83611c42706f296989464dbbe96bf1578b98864a
haojunjie <1373169681@qq.com>
2023-11-04 20:58:18 +0800

    肿瘤登记报告查重

--- src/main/resources/static/tumor/autoCekCan.js
+++ src/main/resources/static/tumor/autoCekCan.js
@@ -2,8 +2,8 @@
 // @name         肿瘤登记报告查重
 // @namespace    http://com.jiem.com/tumor/autoCekCan
 // @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @description  肿瘤登记报告查重提速
+// @author       jiem
 // @match        https://www.bing.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=13.204
 // @license      https://www.apache.org/licenses/LICENSE-2.0
