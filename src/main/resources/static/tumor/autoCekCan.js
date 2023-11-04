014be8a8233c9a848425d1d2d4c329728cc9ed86
haojunjie <1373169681@qq.com>
2023-11-04 20:48:54 +0800

    肿瘤登记报告查重

--- src/main/resources/static/tumor/autoCekCan.js
+++ src/main/resources/static/tumor/autoCekCan.js
@@ -1,15 +1,16 @@
 // ==UserScript==
-// @name         New Userscript
-// @namespace    http://tampermonkey.net/
+// @name         肿瘤登记报告查重
+// @namespace    http://com.jiem.com/tumor/autoCekCan
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.bing.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=13.204
+// @license      https://www.apache.org/licenses/LICENSE-2.0
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
     alert("hallo word");
